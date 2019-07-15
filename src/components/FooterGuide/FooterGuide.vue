2b688231f5e280bfb650814e723307d9974ec2cd
24kcs <younghongbo@126.com>
2019-07-15 12:02:17 +0800

    update-code

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    底部
-  </div>
+  <footer class="footer_guide border-1px">
+    <a href="javascript:;" class="guide_item on">
+      <span class="item_icon">
+        <i class="iconfont icon-waimai"></i>
+      </span>
+      <span>外卖</span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-search"></i>
+      </span>
+      <span>搜索</span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-dingdan"></i>
+      </span>
+      <span>订单</span>
+    </a>
+    <a href="javascript:;" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-geren"></i>
+      </span>
+      <span>我的</span>
+    </a>
+  </footer>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+@import '../../common/stylus/mixins.styl'
+.footer_guide // footer
+  top-border-1px(#e4e4e4)
+  position fixed
+  z-index 100
+  left 0
+  right 0
+  bottom 0
+  background-color #fff
+  width 100%
+  height 50px
+  display flex
+  .guide_item
+    display flex
+    flex 1
+    text-align center
+    flex-direction column
+    align-items center
+    margin 5px
+    color #999999
+    &.on
+      color #02a774
+    span
+      font-size 12px
+      margin-top 2px
+      margin-bottom 2px
+      .iconfont
+        font-size 22px
 </style>
